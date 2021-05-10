d66ed85c5c69607ead4346a4e07852e3c44111af
youben11 <ayouben9@gmail.com>
2021-05-10 16:39:40 +0100

    tests: working trlwe ops on torus elements

--- tests/trlwe/test_trlwe_ops_torus.py
+++ tests/trlwe/test_trlwe_ops_torus.py
@@ -57,53 +57,62 @@ def test_trlwe_add_plain(r1, r2, big_n, k, sigma):
         assert equal_torus_elem(r, e, atol=0.1)
 
 
-@pytest.mark.parametrize("u1", np.arange(0, 1, 0.1).tolist())
-@pytest.mark.parametrize("u2", np.arange(0, 1, 0.1).tolist())
-@pytest.mark.parametrize("n", [600, 800, 1024, 2048])
+@pytest.mark.parametrize("r1", np.arange(0, 1, 0.1).tolist())
+@pytest.mark.parametrize("r2", np.arange(0, 1, 0.1).tolist())
+@pytest.mark.parametrize("big_n", [1024, 2048])
+@pytest.mark.parametrize("k", [1, 2, 3])
 @pytest.mark.parametrize("sigma", [2 ** -15, 2 ** -30])
-def test_trlwe_sub_encrypted(u1, u2, n, sigma):
-    u1 = TorusPolynomial.from_real(u1)
-    u2 = TorusPolynomial.from_real(u2)
-    expected = u1 - u2
-    sk = RLWESecretKey(big_n, k)
+def test_trlwe_sub_encrypted(r1, r2, big_n, k, sigma):
     p = 2 ** 16
-    c1 = TLWE(n, sigma, p)
-    c2 = TLWE(n, sigma, p)
+    u1 = TorusPolynomial.from_real([r1] * big_n, big_n)
+    u2 = TorusPolynomial.from_real([r2] * big_n, big_n)
+    expected = (u1 - u2).to_real(p)
+    sk = RLWESecretKey(big_n, k)
+    c1 = TRLWE(big_n, sigma, p, k)
+    c2 = TRLWE(big_n, sigma, p, k)
     c1.encrypt(sk, u1)
     c2.encrypt(sk, u2)
     c_sub = c1 - c2
     result = c_sub.decrypt(sk).to_real(p)
-    assert equal_torus_elem(result, expected.to_real(p), atol=0.1)
+    assert len(result) == len(expected) == big_n
+    for r, e in zip(result, expected):
+        assert equal_torus_elem(r, e, atol=0.1)
 
 
-@pytest.mark.parametrize("u1", np.arange(0, 1, 0.1).tolist())
-@pytest.mark.parametrize("u2", np.arange(0, 1, 0.1).tolist())
-@pytest.mark.parametrize("n", [600, 800, 1024, 2048])
+@pytest.mark.parametrize("r1", np.arange(0, 1, 0.1).tolist())
+@pytest.mark.parametrize("r2", np.arange(0, 1, 0.1).tolist())
+@pytest.mark.parametrize("big_n", [1024, 2048])
+@pytest.mark.parametrize("k", [1, 2, 3])
 @pytest.mark.parametrize("sigma", [2 ** -15, 2 ** -30])
-def test_trlwe_sub_plain(u1, u2, n, sigma):
-    u1 = TorusPolynomial.from_real(u1)
-    u2 = TorusPolynomial.from_real(u2)
-    expected = u1 - u2
-    sk = RLWESecretKey(big_n, k)
+def test_trlwe_sub_plain(r1, r2, big_n, k, sigma):
     p = 2 ** 16
-    c1 = TLWE(n, sigma, p)
+    u1 = TorusPolynomial.from_real([r1] * big_n, big_n)
+    u2 = TorusPolynomial.from_real([r2] * big_n, big_n)
+    expected = (u1 - u2).to_real(p)
+    sk = RLWESecretKey(big_n, k)
+    c1 = TRLWE(big_n, sigma, p, k)
     c1.encrypt(sk, u1)
     c_sub = c1 - u2
     result = c_sub.decrypt(sk).to_real(p)
-    assert equal_torus_elem(result, expected.to_real(p), atol=0.1)
+    assert len(result) == len(expected) == big_n
+    for r, e in zip(result, expected):
+        assert equal_torus_elem(r, e, atol=0.1)
 
 
-@pytest.mark.parametrize("u1", np.arange(0, 1, 0.1).tolist())
+@pytest.mark.parametrize("r1", np.arange(0, 1, 0.1).tolist())
 @pytest.mark.parametrize("i2", [0, 1, 2, 3, 10])
-@pytest.mark.parametrize("n", [600, 800, 1024, 2048])
+@pytest.mark.parametrize("big_n", [1024, 2048])
+@pytest.mark.parametrize("k", [1, 2, 3])
 @pytest.mark.parametrize("sigma", [2 ** -15, 2 ** -30])
-def test_trlwe_mul_int(u1, i2, n, sigma):
+def test_trlwe_mul_int(r1, i2, big_n, k, sigma):
     p = 2 ** 16
-    u1 = Torus.from_real(u1)
-    expected = u1 * i2
+    u1 = TorusPolynomial.from_real([r1] * big_n, big_n)
+    expected = (u1 * i2).to_real(p)
     sk = RLWESecretKey(big_n, k)
-    c1 = TLWE(n, sigma, p)
+    c1 = TRLWE(big_n, sigma, p, k)
     c1.encrypt(sk, u1)
     c_mul = c1 * i2
     result = c_mul.decrypt(sk).to_real(p)
-    assert equal_torus_elem(result, expected.to_real(p), atol=0.1)
+    assert len(result) == len(expected) == big_n
+    for r, e in zip(result, expected):
+        assert equal_torus_elem(r, e, atol=0.1)
