a844ce3d97166017c84b94fd6cf83cb80b178a35
youben11 <ayouben9@gmail.com>
2021-05-14 10:20:25 +0100

    correct polymul and polymod

--- tfhe/poly.py
+++ tfhe/poly.py
@@ -1,18 +1,20 @@
 import numpy as np
-from numpy.polynomial import polynomial as poly
-
-
-def _get_pmod_from_big_n(big_n=1024):
-    poly_mod = [1] + [0] * (big_n - 1) + [1]
-    return np.uint64(poly_mod)
 
 
 def polymod(p, big_n, q=2 ** 64):
-    # TODO: compute polymod using rotate and negate
-    poly_mod = _get_pmod_from_big_n(big_n)
-    return np.uint64(poly.polydiv(np.int64(p), poly_mod)[1])
+    # assume polynomial modulus is always X^N + 1
+    chunks = [np.uint64(p[i : i + big_n]) for i in range(0, len(p), big_n)]
+    # padd last chunk
+    if len(chunks[-1]) != big_n:
+        chunks[-1] = np.uint64(chunks[-1].tolist() + [0] * (big_n - len(chunks[-1])))
+    sign = np.uint64(1)
+    acc = np.zeros_like(chunks[0])
+    for chunk in chunks:
+        acc += chunk * sign
+        sign *= np.uint64(-1)
+    return acc
 
 
 def polymul(p1, p2, q=2 ** 64):
-    # TODO: use old polu API of numpy to compute polymul with uint64
-    return np.uint64([s * a for s, a in zip(p1, p2)])
+    # using old API of numpy since it allows polymul of uint64
+    return np.polymul(p1[::-1], p2[::-1])[::-1]
